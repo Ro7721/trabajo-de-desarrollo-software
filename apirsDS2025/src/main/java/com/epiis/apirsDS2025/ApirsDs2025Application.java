d754e96a848c61af90ad516b3d5a93746f500287
ROGER <232175@unamba.edu.pe>
2025-11-13 09:58:37 -0500

    Subiendo carpeta como nuevo proyecto

---
+++ apirsDS2025/src/main/java/com/epiis/apirsDS2025/ApirsDs2025Application.java
@@ -0,0 +1,13 @@
+package com.epiis.apirsDS2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirsDs2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirsDs2025Application.class, args);
+	}
+
+}
---
+++ apirsDS2025/src/test/java/com/epiis/apirsDS2025/ApirsDs2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epiis.apirsDS2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirsDs2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
